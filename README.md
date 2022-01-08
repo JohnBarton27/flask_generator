89c5e65b4a49ba60d14c951323ef416bbd92dd57
John Barton <john@mediayoucanfeel.com>
2022-01-07 20:26:06 -0500

    Add initial support for creating project directory & requirements.txt file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Flask Generator
 Simple script that creates a new Flask application (as a git repository) given several inputs from the user.
+
+## Running
+Run `python main.py`.
---
+++ main.py
@@ -0,0 +1,44 @@
+import os
+import textwrap
+
+
+def get_project_dir():
+    directory_location = os.path.expanduser(input('Where you do you want to put the project folder? '))
+    project_directory = os.path.join(directory_location, repo_slug)
+    if not os.path.exists(directory_location):
+        raise FileNotFoundError(f'Project location ({directory_location}) must be an existing location!')
+
+    if os.path.exists(project_directory):
+        raise FileExistsError(f'Project directory {project_directory} already exists!')
+
+    print(f'Creating project directory \'{project_directory}\'...')
+    os.mkdir(project_directory)
+    return project_directory
+
+
+def create_requirements():
+    req_content = """\
+        flask
+        xmlrunner
+        coverage
+        pynput
+    """
+
+    write_to_file('requirements.txt', req_content)
+
+
+def write_to_file(filepath, content):
+    global project_dir
+    full_path = os.path.join(project_dir, filepath)
+
+    with open(full_path, 'w') as file_to_write:
+        file_to_write.write(textwrap.dedent(content))
+
+
+# Base information
+project_name = input('Project name: ')
+repo_slug = project_name.lower().replace(' ', '-')
+project_dir = get_project_dir()
+create_requirements()
+
+print(f'Creating project \'{project_name}\'...')
